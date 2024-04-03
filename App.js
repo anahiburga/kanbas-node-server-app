1b16784ab80a16d4ba8025ffed212527939edd86
anahi <anahiburga@anahis-laptop.local>
2024-04-02 23:26:10 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from "cors";
+import Hello from "./Hello.js"
+import Lab5 from "./Lab5.js";
+import CourseRoutes from './Kanbas/Courses/routes.js';
+import ModuleRoutes from "./Kanbas/modules/routes.js";
+
+const app = express()
+app.use(cors());
+app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
+Hello(app)
+Lab5(app);
+app.listen(process.env.PORT || 4000);
---
+++ Kanbas/Database/courses.js
@@ -0,0 +1,14 @@
+export default [
+    { 
+        "_id": "CS4550", "name": "Web Development", "number": "CS4550", "startDate": "2024-01-10",
+        "endDate": "2023-05-15", "image": "reactjs.png"  
+    },
+    { 
+        "_id": "CS4400", "name": "Programming Languages", "number": "CS4400", "startDate": "2024-01-10",
+        "endDate": "2023-05-15", "image": "pl.png"  
+    },
+    { 
+        "_id": "DS3000", "name": "Foundations of Data Science", "number": "DS3000", "startDate": "2024-01-10",
+        "endDate": "2023-05-15", "image": "ds.png"  
+    } 
+]
